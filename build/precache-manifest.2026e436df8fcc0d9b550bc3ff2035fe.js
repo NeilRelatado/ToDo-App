9d7362b7bab41f270d3b64b86cb7f1513b97f18f
NeilRelatado <neilrelatado@live.com>
2019-07-22 22:57:36 -0400

    ToDo List application made with React.js

---
+++ build/precache-manifest.2026e436df8fcc0d9b550bc3ff2035fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8be1c892d8cf30f8a1d110ab79ad53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00f089913e8449439f86",
+    "url": "/static/css/main.c1eb43f7.chunk.css"
+  },
+  {
+    "revision": "2c9294f145e9fff1a5ea",
+    "url": "/static/js/2.3611b442.chunk.js"
+  },
+  {
+    "revision": "00f089913e8449439f86",
+    "url": "/static/js/main.8acc76cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
